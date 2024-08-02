c8f0e34d52274b0631fdd8c669d1273f50694a56
fsalle1954 <fsalle2002@gmail.com>
2024-08-02 15:58:17 +0200

    chakra 3

---
+++ next.config.js
@@ -0,0 +1,12 @@
+module.exports = {
+  reactStrictMode: true,
+  images: {
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "bayut-production.s3.eu-central-1.amazonaws.com",
+        hostname: "**",
+      },
+    ],
+  },
+};
