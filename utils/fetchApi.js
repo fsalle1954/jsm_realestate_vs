f303e2497dfd3e7f4a1b0d6c5280bd5901286d14
fsalle1954 <fsalle2002@gmail.com>
2024-08-06 13:26:27 +0200

    fetchApi 56

--- utils/fetchApi.js
+++ utils/fetchApi.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-export const baseUrl = "https://bayut-com1.p.rapidapi.com";
+export const baseUrl = "https://bayut.p.rapidapi.com";
 
 export const fetchApi = async (url) => {
   const { data } = await axios.get(url, {
@@ -12,7 +12,7 @@ export const fetchApi = async (url) => {
     },
     headers: {
       "x-rapidapi-key": "0046fb64bbmsh8f8f6c89ee5ef99p1dcf84jsn258c853c2668",
-      "x-rapidapi-host": "bayut-com1.p.rapidapi.com",
+      "x-rapidapi-host": "bayut.p.rapidapi.com",
     },
   });
   return data;
